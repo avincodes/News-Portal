7882119f8872d8f4d040ba91bd941e1ab9bd4e98
Niyas C <niyasc@localhost.localdomain>
2013-06-05 16:46:05 +0530

    Frontend partially done

--- templates/home.php
+++ templates/home.php
@@ -1,34 +1,103 @@
-<table class="center">
+<table class="news-table">
+<?php
+	$latest=query("select id,title,content from news order by timestamp desc limit 5;");
+?>
 	<tr>
 		<td>
 			<table>
 				<tr>
-					<td>
-						First News
+					<td style="vertical-align:top">
+						<table class="first-news">
+						<?php 
+							$current=$latest[0];
+							$image=query("select address from images where id=?",$current["id"]);
+							if(count($image)==0)
+							{
+								$image="default.png";
+							}
+							else
+							{
+								$image=$image[0];
+								$image=$image["address"];
+							}
+						?>
+							<tr>
+								<td style="height:300px;text-align:center">
+									<?php
+										$dim=getimagesize("images/".$image);
+										//print_r($dim);
+										$height=$dim[1];
+										$width=$dim[0];
+										$ar=$width/$height;
+										if($ar>2)
+										{
+											$width=600;
+											$height=300/$ar;
+										}
+										else
+										{
+											$height=300;
+											$width=300*$ar;
+										}
+										print "<img src=images/".$image." height=".$height." width=".$width." />";
+									?>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<table class="first-news">
+										<tr>
+											<td class="title">
+												<?=$current["title"]?>
+											</td>
+										</tr>
+										<tr>
+											<td class="content">
+												<?=substr($current["content"],0,768)?>...
+											</td>
+										</tr>
+									</table>	
+								</td>
+							</tr>
+						</table>	
 					</td>
-					<td>
+					<td style="vertical-align:top">
 						<table>
+						<?php 
+							$latest=array_slice($latest,1);
+							foreach($latest as $current)
+							{
+						?>
 							<tr>
 								<td>
-									Second News
+									<table class="latest-news">
+										<tr>
+											<td class="title">
+												<?=$current["title"]?>	
+											</td>
+										</tr>
+										<tr>
+											<td class="content">
+												<?=substr($current["content"],0,256)?>...
+											</td>
+										</tr>
+									</table>
 								</td>
 							</tr>
-							<tr>
-								<td>
-									Third News
-								</td>
-							</tr>
+						<?php
+							}
+						?>
 						</table>
 					</td>
 				</tr>
 			</table>
 		</td>
-	</td>
+	</tr>
 	<tr>
 		<td>
 			Preview;
 		</td>
-	</td>
+	</tr>
 	<?php
 		$categories=query("select * from categories");
 		$n=count($categories);
